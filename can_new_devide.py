9c78a750e957a073f109b621a1f791c4a972a795
Andrew <rendezvous330@gmail..com>
2023-12-19 16:28:42 +0900

    Feat : Divide serial port selection class to Python file.

--- can_new_devide.py
+++ can_new_devide.py
@@ -2,7 +2,6 @@ import os
 import sys
 import can
 import time
-import serial.tools.list_ports
 from datetime import datetime
 import security_algorithm as algo
 import data_identifier as data_id
@@ -18,6 +17,7 @@ import can_thread as worker
 import pyqtgraph as pg
 import user_filter
 import user_signal
+import sereal_port_selection
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 Ui_MainWindow, QtBaseClass = pg.Qt.loadUiType(BASE_DIR + r"./src/can_basic_ui.ui")
@@ -1173,7 +1173,7 @@ class Main(QMainWindow, Ui_MainWindow):
                 self.serial_selection_obj.ui_close()
         else:
             if not self.serial_selection_obj:
-                self.serial_selection_obj = Serial_Port_Selection()
+                self.serial_selection_obj = sereal_port_selection.SerialPortSelection(BASE_DIR, self)
                 self.serial_selection_obj.search_serial()
             else:
                 self.serial_selection_obj.ui_open()
@@ -3041,64 +3041,6 @@ class Diag_Main(QDialog):
 
     def ui_close(self):
         self.close()
-
-
-class Serial_Port_Selection(QDialog):
-
-    def __init__(self):
-        super().__init__()
-        self.ui = uic.loadUi(BASE_DIR + r"./src/can_serial_bus_selection_ui.ui", self)
-
-        self.setWindowTitle("Serial Port Selection for E-51 IVI CAN Simulator")
-        self.ui_open()
-
-        self.port_chkboxes = [self.chkbox_serial_port_1, self.chkbox_serial_port_2,
-                              self.chkbox_serial_port_3, self.chkbox_serial_port_4,
-                              self.chkbox_serial_port_5, self.chkbox_serial_port_6]
-
-        self.btn_ports_apply.clicked.connect(self.selected_port_handler)
-        self.btn_ports_refresh.clicked.connect(self.search_serial)
-
-        self.ui_visibility()
-
-    def port_setting(self, port_list=None):
-        if len(port_list) != 0:
-            self.ui_visibility(True)
-            for num, device in zip(range(len(port_list)), port_list):
-                self.port_chkboxes[num].setEnabled(True)
-                self.port_chkboxes[num].setText(device.device)
-        else:
-            self.ui_visibility(False)
-
-    def ui_visibility(self, flag=True):
-        for device in self.port_chkboxes:
-            device.setText("Serial port")
-            device.setVisible(flag)
-            device.setChecked(False)
-        self.label_no_port.setVisible(not flag)
-
-    def ui_open(self):
-        self.show()
-
-    def ui_close(self):
-        self.close()
-
-    def search_serial(self):
-        ports = serial.tools.list_ports.comports()
-        if len(ports) == 0:
-            mywindow.serial_devices = []
-        else:
-            mywindow.serial_devices = ports
-        self.port_setting(mywindow.serial_devices)
-
-    def selected_port_handler(self):
-        selected_list = []
-        for port in self.port_chkboxes:
-            if port.isChecked():
-                selected_list.append(port.text())
-        mywindow.selected_ports = selected_list
-        mywindow.bus_console.appendPlainText("Serial port is selected")
-        self.ui_close()
 
 
 if __name__ == '__main__':
---
+++ sereal_port_selection.py
@@ -0,0 +1,63 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+import serial.tools.list_ports
+
+
+class SerialPortSelection(QDialog):
+
+    def __init__(self, base_dir, main_obj):
+        super().__init__()
+        self.ui = uic.loadUi(base_dir + r"./src/can_serial_bus_selection_ui.ui", self)
+
+        self.setWindowTitle("Serial Port Selection for E-51 IVI CAN Simulator")
+        self.ui_open()
+
+        self.main_obj = main_obj
+
+        self.port_chkboxes = [self.chkbox_serial_port_1, self.chkbox_serial_port_2,
+                              self.chkbox_serial_port_3, self.chkbox_serial_port_4,
+                              self.chkbox_serial_port_5, self.chkbox_serial_port_6]
+
+        self.btn_ports_apply.clicked.connect(self.selected_port_handler)
+        self.btn_ports_refresh.clicked.connect(self.search_serial)
+
+        self.ui_visibility()
+
+    def port_setting(self, port_list=None):
+        if len(port_list) != 0:
+            self.ui_visibility(True)
+            for num, device in zip(range(len(port_list)), port_list):
+                self.port_chkboxes[num].setEnabled(True)
+                self.port_chkboxes[num].setText(device.device)
+        else:
+            self.ui_visibility(False)
+
+    def ui_visibility(self, flag=True):
+        for device in self.port_chkboxes:
+            device.setText("Serial port")
+            device.setVisible(flag)
+            device.setChecked(False)
+        self.label_no_port.setVisible(not flag)
+
+    def ui_open(self):
+        self.show()
+
+    def ui_close(self):
+        self.close()
+
+    def search_serial(self):
+        ports = serial.tools.list_ports.comports()
+        if len(ports) == 0:
+            self.main_obj.serial_devices = []
+        else:
+            self.main_obj.serial_devices = ports
+        self.port_setting(self.main_obj.serial_devices)
+
+    def selected_port_handler(self):
+        selected_list = []
+        for port in self.port_chkboxes:
+            if port.isChecked():
+                selected_list.append(port.text())
+        self.main_obj.selected_ports = selected_list
+        self.main_obj.bus_console.appendPlainText("Serial port is selected")
+        self.ui_close()
